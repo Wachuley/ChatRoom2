a3bf9ee4187112ae4cbc5cec1dc9a7c6c2a3ced4
Mabel <dmabelmb04@gmail.com>
2025-09-30 16:47:16 -0600

    Validación de usuario en Inicio de Sesión

--- ChatRoom/Form2.Designer.cs
+++ ChatRoom/Form2.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.Usuario = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Contraseña = new System.Windows.Forms.TextBox();
-            this.TextUsuario = new System.Windows.Forms.Label();
-            this.TextContraseña = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.IngresarBtn = new System.Windows.Forms.Button();
+            this.Mensaje = new System.Windows.Forms.Label();
+            this.TextContraseña = new System.Windows.Forms.Label();
+            this.TextUsuario = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,7 +50,7 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.IngresarBtn);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Mensaje);
             this.panel1.Controls.Add(this.TextContraseña);
             this.panel1.Controls.Add(this.TextUsuario);
             this.panel1.Controls.Add(this.Contraseña);
@@ -60,23 +60,25 @@
             this.panel1.Size = new System.Drawing.Size(500, 387);
             this.panel1.TabIndex = 1;
             // 
-            // Contraseña
+            // IngresarBtn
             // 
-            this.Contraseña.Location = new System.Drawing.Point(120, 240);
-            this.Contraseña.Multiline = true;
-            this.Contraseña.Name = "Contraseña";
-            this.Contraseña.Size = new System.Drawing.Size(260, 34);
-            this.Contraseña.TabIndex = 1;
+            this.IngresarBtn.Location = new System.Drawing.Point(174, 327);
+            this.IngresarBtn.Name = "IngresarBtn";
+            this.IngresarBtn.Size = new System.Drawing.Size(151, 42);
+            this.IngresarBtn.TabIndex = 2;
+            this.IngresarBtn.Text = "Ingresar";
+            this.IngresarBtn.UseVisualStyleBackColor = true;
+            this.IngresarBtn.Click += new System.EventHandler(this.IngresarBtn_Click);
             // 
-            // TextUsuario
+            // Mensaje
             // 
-            this.TextUsuario.AutoSize = true;
-            this.TextUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextUsuario.Location = new System.Drawing.Point(115, 114);
-            this.TextUsuario.Name = "TextUsuario";
-            this.TextUsuario.Size = new System.Drawing.Size(85, 25);
-            this.TextUsuario.TabIndex = 2;
-            this.TextUsuario.Text = "Usuario:";
+            this.Mensaje.AutoSize = true;
+            this.Mensaje.Location = new System.Drawing.Point(116, 289);
+            this.Mensaje.Name = "Mensaje";
+            this.Mensaje.Size = new System.Drawing.Size(244, 20);
+            this.Mensaje.TabIndex = 4;
+            this.Mensaje.Text = "(aquí va contraseña incorrecta :p)";
+            this.Mensaje.Click += new System.EventHandler(this.label1_Click);
             // 
             // TextContraseña
             // 
@@ -88,25 +90,23 @@
             this.TextContraseña.TabIndex = 3;
             this.TextContraseña.Text = "Contraseña:";
             // 
-            // label1
+            // TextUsuario
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(116, 289);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(244, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "(aquí va contraseña incorrecta :p)";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.TextUsuario.AutoSize = true;
+            this.TextUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextUsuario.Location = new System.Drawing.Point(115, 114);
+            this.TextUsuario.Name = "TextUsuario";
+            this.TextUsuario.Size = new System.Drawing.Size(85, 25);
+            this.TextUsuario.TabIndex = 2;
+            this.TextUsuario.Text = "Usuario:";
             // 
-            // IngresarBtn
+            // Contraseña
             // 
-            this.IngresarBtn.Location = new System.Drawing.Point(174, 327);
-            this.IngresarBtn.Name = "IngresarBtn";
-            this.IngresarBtn.Size = new System.Drawing.Size(151, 42);
-            this.IngresarBtn.TabIndex = 2;
-            this.IngresarBtn.Text = "Ingresar";
-            this.IngresarBtn.UseVisualStyleBackColor = true;
-            this.IngresarBtn.Click += new System.EventHandler(this.IngresarBtn_Click);
+            this.Contraseña.Location = new System.Drawing.Point(120, 240);
+            this.Contraseña.Multiline = true;
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(260, 34);
+            this.Contraseña.TabIndex = 1;
             // 
             // IniciarSesion
             // 
@@ -129,7 +129,7 @@
         private System.Windows.Forms.TextBox Contraseña;
         private System.Windows.Forms.Label TextUsuario;
         private System.Windows.Forms.Button IngresarBtn;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Mensaje;
         private System.Windows.Forms.Label TextContraseña;
     }
 }
